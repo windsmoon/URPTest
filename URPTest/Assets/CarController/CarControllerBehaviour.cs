b0ed1df4227e494a9f0a3147ff81236afba77cb8
DESKTOP-4NRNDS0\windsmoon <smwindsmoon@163.com>
2020-12-31 12:52:56 +0800

    demo car controller

---
+++ URPTest/Assets/CarController/CarControllerBehaviour.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CarController
+{
+    public class CarControllerBehaviour : MonoBehaviour
+    {
+        #region fields
+        [SerializeField] 
+        private Camera camera;
+        [SerializeField]
+        private float speed = 15;
+        [SerializeField]
+        private float rotateSpeed = 45f;
+        [SerializeField]
+        private float cameraMoveTime = 1;
+        private Vector3 cameraTargetPos;
+        #endregion
+        
+        #region unity methods
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.up, -rotateSpeed * Time.deltaTime, Space.Self);
+            }
+            
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.Self);
+            }
+            
+        }
+
+        private void LateUpdate()
+        {
+            cameraTargetPos = transform.position + new Vector3(0, 6, -10);
+            Vector3 posOffset = cameraTargetPos - camera.transform.position;
+            Vector3 currentCameraSpeed = posOffset / cameraMoveTime;
+            camera.transform.position = camera.transform.position + currentCameraSpeed * Time.deltaTime; 
+            camera.transform.LookAt(transform, Vector3.up);
+        }
+        #endregion
+    } 
+}
+
