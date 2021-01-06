ccc51d64f65b409a7777bf61dd1c6c4513454f95
DESKTOP-4NRNDS0\windsmoon <smwindsmoon@163.com>
2021-01-06 13:34:22 +0800

    update todo list

--- URPTest/Assets/TodoList.cs
+++ URPTest/Assets/TodoList.cs
@@ -4,8 +4,8 @@
 // 3 : not urgent
 
 // clear : sample environment indirect light
-// support depth and normals texture
-// 0 waiting test : support alpha test
+// clear : support depth and normals texture
+// clear : support alpha test
 // 1 : add the cel shading
 // 1 : support pre depth for alpha test performance
 // 3 : make the japanese class room demo beautiful
