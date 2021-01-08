65f9b06235343c9af7570879af77cf03f1e0090c
windsmoon <smwindsmoon@gmail.com>
2021-01-09 01:23:29 +0800

    cel shading

--- URPTest/Assets/CelPBR/Shader/Lighting.hlsl
+++ URPTest/Assets/CelPBR/Shader/Lighting.hlsl
@@ -13,35 +13,6 @@
 #if defined(CEL_SHADING)
 real3 GetLighting(LightData_CelPBR lightData, Surface_CelPBR surface, BRDF_CelPBR brdf, TempData_CelPBR tempData)
 {
-    // return (brdf.diffuse + brdf.specular) * lightData.color * lightData.distanceAttenuation * lightData.shadowAttenuation * tempData.nDotL;
-    //
-    // float nDotL = dot(surface.normal, lightData.direction);
-    // float nDotV = dot(surface.normal, tempData.viewDirection);
-    //
-    // // diffuse
-    // float halfLambert = nDotL * 0.5 + 0.5;
-    // half ramp = GetRamp(halfLambert * lightData.distanceAttenuation * (saturate(lightData.shadowAttenuation + 0)) - GetShadowRange()); // todo : edge light ?
-    // // half ramp = smoothstep(0, _ShadowSmooth, halfLambert -  GetShadowRange());
-    // // float3 diffuse = halfLambert >  GetShadowRange() ? GetCelShadeColor() : GetShadowColor();
-    // float3 diffuse = lerp(GetShadowColor(), GetCelShadeColor(), ramp);
-    //
-    // // rim light
-    // float f = 1 - tempData.nDotV;
-    // // f = f * (nDotL * 0.5 + 0.5); // (wnDotL * 0.5 + 0.5) make rim light range bigger
-    // f = f * tempData.nDotL;
-    // float2 rimRange = GetRimRange();
-    // float4 rimColor = GetRimColor();
-    // f = smoothstep(rimRange.x, rimRange.y, f);
-    // float3 rim = f * rimColor.rgb;
-    // rim = lerp(brdf.reflectivity, rim, rimColor.a);
-    // real3 color = (diffuse * brdf.diffuse + brdf.specular + rim) * lightData.color;
-    // // float3 color = (diffuse * GetDirectBRDF(surface, brdfLight, light) + rim) * light.color;
-    // return color;
-
-    
-    // float nDotL = dot(surface.normal, lightData.direction);
-    // float nDotV = dot(surface.normal, tempData.viewDirection);
-
     // diffuse
     half ramp = GetRamp(tempData.nDotL - GetShadowRange()); // todo : edge light ?
     // half ramp = smoothstep(0, _ShadowSmooth, halfLambert -  GetShadowRange());
