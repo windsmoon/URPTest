aab2723beda708c75181420c4c0d446d5794ff87
windsmoon <smwindsmoon@gmail.com>
2021-02-27 22:02:15 +0800

    Update TodoList.cs

--- URPTest/Assets/TodoList.cs
+++ URPTest/Assets/TodoList.cs
@@ -5,9 +5,11 @@
 // clear : sample environment indirect light
 // clear : support depth and normals texture
 // clear : support alpha test
-// 1 doing : add the cel shading
+// clear : add the cel shading
 // 1 : support pre depth for alpha test performance
 // 1 doing : make a demo
 // clear : add simple sss effect
 // 1 doing add kk highlight
 // 1 add character self shadow
+// 1 palnar reflection
+// 1 ssr
