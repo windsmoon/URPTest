18d221982a744d58a57321cd6da4d280ac43d0b4
windsmoon <smwindsmoon@gmail.com>
2021-03-12 19:09:56 +0800

    Update ScreenSpaceRelfectionRenderPass.cs

--- URPTest/Assets/CelPBR/Runtime/PostProcessing/RenderPasses/ScreenSpaceRelfectionRenderPass.cs
+++ URPTest/Assets/CelPBR/Runtime/PostProcessing/RenderPasses/ScreenSpaceRelfectionRenderPass.cs
@@ -1,14 +1,28 @@
-﻿using UnityEngine.Rendering;
+﻿using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 namespace CelPBR.Runtime.PostProcessing.RenderPasses
 {
     public class ScreenSpaceRelfectionRenderPass : PostProcessingRenderPass
     {
+        #region constants
+        #endregion
+
+        #region fields
+        // private int 
+        #endregion
+        
         #region methods
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
         }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            base.Configure(cmd, cameraTextureDescriptor);
+        }
+
         #endregion
     }
 }
