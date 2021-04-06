0047cd42f4d31931a3069f1dc411868d70560321
windsmoon <smwindsmoon@gmail.com>
2021-04-06 19:13:52 +0800

    Update TodoList.cs

--- URPTest/Assets/TodoList.cs
+++ URPTest/Assets/TodoList.cs
@@ -2,33 +2,50 @@
 // 1 : important
 // 2 : normal
 
+
+// update urp version
+// 1 : supprot screen space shadow map in urp 11
+// 1 : support point light shadow in urp 11
+
+// long-term objectives
+// 1 doing : make a demo
+
+// common render
+// 1 : support pre depth for alpha test performance
 // clear : sample environment indirect light
 // clear : support depth and normals texture
 // clear : support alpha test
-// clear : add the cel shading
-// clear : add simple sss effect
 
+// character shading
+// clear : add the cel shading
+// clear : add kk highlight
+// clear : add simple sss effect
+// 1 : add character self shadow
+// 2 : cel shading double layer kk high light
+
+// environment
+// 1 : fft water
+// 1 : water reflection
+// 1 : water refraction
+
+// post processing
 // clear : post processing system
-// 1 : support pre depth for alpha test performance
-// 1 doing : make a demo
-// 1 doing add kk highlight
-// 1 add character self shadow
-// 1 palnar reflection
-// 1 ssr
-// 1 optimize the SSROBJECT shader
-// 1 ssr sample skybox
-// 1 fft water
-// 1 water reflection
-// 1 water refraction
-// 2 post processing config inspector
-// 2 post processing support selecting camera event
-// 2 post processing support opaque texture and depth texture
-// 2 post processing need opaque and depth texutre
-// 2 post processing need uber render pass 
-// 2 post processing context.submit
-// 2 post processing need pre post processing render pass 
+// 2 : post processing config inspector
+// 2 : post processing support selecting camera event
+// 2 : post processing support opaque texture and depth texture
+// 2 : post processing need opaque and depth texutre
+// 2 : post processing need uber render pass 
+// 2 : post processing context.submit
+// 2 : post processing need pre post processing render pass 
+
+// ssr
+// 1 doing : ssr
+// 1 : ssr sample skybox
+// 1 : optimize the SSROBJECT shader
+
+// planar reflection
+// clear : planar reflection
+
+// outline
 // clear : post processing outline optimal (use depth and normal)
-// 2 post processing outline depth threshold use near and far clip plane
-// 2 cel shading double layer kk high light
-// 0 : supprot screen space shadow map in urp 11
-// 1 : support point light shadow in urp 11
+// 2 : post processing outline depth threshold use near and far clip plane
