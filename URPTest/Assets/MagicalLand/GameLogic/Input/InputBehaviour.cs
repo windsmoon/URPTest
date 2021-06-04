a5683c69e0b98492e86278b12f483e6847bd9105
windsmoon <smwindsmoon@gmail.com>
2021-06-04 19:27:30 +0800

    input

---
+++ URPTest/Assets/MagicalLand/GameLogic/Input/InputBehaviour.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagicalLand.GameLogic
+{
+    public class InputBehaviour : MonoBehaviour
+    {
+        #region unity methods
+        private void Update()
+        {
+        }
+        #endregion
+
+        #region methods
+
+        public void OnMove()
+        {
+        }
+        #endregion
+    }
+}
+  
