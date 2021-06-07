7f6fe12d3c60da2d74e7086b236a1868bd0ae824
windsmoon <smwindsmoon@gmail.com>
2021-06-07 20:34:04 +0800

    character

--- URPTest/Assets/MagicalLand/GameLogic/Scene/MainSceneController.cs
+++ URPTest/Assets/MagicalLand/GameLogic/Scene/MainSceneController.cs
@@ -14,7 +14,7 @@ namespace MagicalLand.GameLogic.Scene
         #region constructors
         public MainSceneController(int id, UnityEngine.SceneManagement.Scene scene) : base(id, scene)
         {
-            playerTransform = rootTransform.Find("ReisalinStout"); 
+            playerTransform = rootTransform.Find("Reisalin-unity"); 
             this.characterController = new CharacterController(playerTransform);
         }
         #endregion
