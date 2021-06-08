2f7b6464fcf6c77492426b86e10c4b81826fb817
windsmoon <smwindsmoon@gmail.com>
2021-06-08 16:10:02 +0800

    character move

--- URPTest/Assets/MagicalLand/GameLogic/Scene/MainSceneController.cs
+++ URPTest/Assets/MagicalLand/GameLogic/Scene/MainSceneController.cs
@@ -14,7 +14,7 @@ namespace MagicalLand.GameLogic.Scene
         #region constructors
         public MainSceneController(int id, UnityEngine.SceneManagement.Scene scene) : base(id, scene)
         {
-            playerTransform = rootTransform.Find("LysaTest"); 
+            playerTransform = rootTransform.Find("reisalin-whiteT-unity"); 
             this.characterController = new CharacterController(playerTransform);
         }
         #endregion
